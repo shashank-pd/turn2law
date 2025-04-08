79d1f21ddd48a00aa19e26e027616923a2d59bbf
shashank-pd <shashankpd2606@gmail.com>
2025-04-08 22:21:28 +0530

    update layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import Navbar from '../components/Navbar'
 import Footer from '../components/footer'
+import { Toaster } from 'react-hot-toast' // Only this is needed
 
 export const metadata = {
   title: 'Turn2Law',
@@ -12,7 +13,8 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className="flex flex-col min-h-screen">
         <Navbar />
-        <main className="flex-grow">
+        <Toaster position="top-center" reverseOrder={false} />
+        <main className="grow">
           {children}
         </main>
         <Footer />
