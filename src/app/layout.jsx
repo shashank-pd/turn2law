65665b95c6f3a164c140c3fa33e9f6cc62541f18
shashank-pd <shashankpd2606@gmail.com>
2025-04-09 15:07:35 +0530

    update layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,12 +1,12 @@
-import './globals.css'
-import Navbar from '../components/Navbar'
-import Footer from '../components/footer'
-import { Toaster } from 'react-hot-toast' // Only this is needed
+import './globals.css';
+import Navbar from '../components/Navbar';
+import Footer from '../components/footer';
+import { Toaster } from 'react-hot-toast'; // using react-hot-toast
 
 export const metadata = {
   title: 'Turn2Law',
   description: 'Instant Legal Help & AI Assistant',
-}
+};
 
 export default function RootLayout({ children }) {
   return (
@@ -20,5 +20,5 @@ export default function RootLayout({ children }) {
         <Footer />
       </body>
     </html>
-  )
+  );
 }
